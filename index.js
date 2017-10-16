1834a4be0d20c1efbf911045470a1f817455d284
IagoLast <iago.lastra@gmail.com>
2017-10-16 23:07:01 +0200

    Use cache module instead reinvent the wheel ;)

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const app = require('express')();
 const api = require('./src/api.js');
-
-const cache = require('./src/cache.js');
-
 const cors = require('cors')
 const PORT = process.env.PORT || 8080;
+const apicache = require('apicache');
 
+app.use(apicache.middleware('4 hour'))
+app.use(cors());
 app.set('json spaces', 2);
-app.use(cors())
-app.use('/api/v1/', cache);
-app.use('/api/v1/', api);
+
+app.get('/api/v1/simple/:id', api);
 
 app.listen(PORT);
 
--- src/api.js
+++ src/api.js
@@ -1,22 +1,7 @@
 const surfguru = require('surfguru-parser');
-const express = require('express');
-const router = express.Router();
-const cache = require('memory-cache');
-const TTL = 1800000; // half an hour
 
-router.get('/simple/:id', function (req, res) {
-	console.log('Entering backend');
+module.exports = function (req, res) {
 	surfguru(req.params.id)
-		.then(data => {
-			cache.put(req.originalUrl, data, TTL);
-			res.json(data);
-		})
-		.catch(err => {
-			console.error(err);
-			res.status(500).json();
-		});
-
-});
-
-
-module.exports = router;
+		.then(data => res.json(data))
+		.catch(err => res.status(500).json());
+}
